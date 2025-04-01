1a2ebb04428cf6f42a2190c81b109cc073d3ff01
AnticcX <45725948+AnticcX@users.noreply.github.com>
2025-03-31 22:24:33 -0700

    added reset command
    Resets the conversation

---
+++ bot/features/reset_convo.py
@@ -0,0 +1,28 @@
+import discord
+
+from discord.ext import commands
+from discord import app_commands
+from ..response_manager import reset_conversation
+
+MY_GUILD = discord.Object(id=764920345282084865)
+
+class resetConvo(commands.Cog):
+    def __init__(self, client: commands.Bot):
+        self.client = client
+    
+    @app_commands.command(
+        name="reset", 
+        description="Resets the user's current conversation"
+        )
+    async def reset(self, interaction: discord.Interaction):
+        reset_conversation(str(interaction.user.id))
+        await interaction.response.send_message(f"{interaction.user.mention} was resetted!")
+        
+    async def cog_load(self):
+        # Attach command directly to the tree
+        self.client.tree.add_command(self.reset, guild=MY_GUILD)
+        print("✅ Slash command added to tree")
+        
+async def setup(client: commands.Bot):
+    await client.add_cog(resetConvo(client))
+    print(f'Cog {__name__} loaded!')
